d58d45038984bc0f9edc2a202cebd98409ce0238
marimugesh <marimugesh23@gmail.com>
2023-10-20 21:01:07 +0530

    Successfully Task Commited

---
+++ src/main/java/com/log4j/Log4jApplication.java
@@ -0,0 +1,36 @@
+package com.log4j;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+
+@SpringBootApplication
+public class Log4jApplication {
+	
+	static Logger log = Logger.getLogger(Log4jApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jApplication.class, args);
+		
+		
+		BasicConfigurator.configure();
+		log.info("Informtion");
+		log.error("Error");
+		log.warn("Warning");
+		log.fatal("Critical Error");
+		log.debug("Debugging");
+		
+		PropertyConfigurator.configure("log4j.properties"  );
+		log.info("Informtion");
+		log.error("Error");
+		log.warn("Warning");
+		log.fatal("Critical Error");
+		log.debug("Debugging");
+	}
+
+}
